5f438b5a4dcd065332e1a7f74a877e117f0e91a3
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-04-07 11:32:40 +0300

    Update example

--- examples/simple.py
+++ examples/simple.py
@@ -1,28 +1,15 @@
 import asyncio
-from pgtulip.pool import Pool
+import aiopg
 
 dsn = 'dbname=jetty user=nick password=1234 host=localhost'
 
 
 @asyncio.coroutine
 def test_select():
-    pool = Pool(dsn)
-    # init pool connections
-    yield from pool.setup()
-    morgify_result = yield from pool.morgify("select 1;")
-    # simple query in transaction
-    transaction_result = yield from pool.run_transaction(
-        [
-            ("select %s;", (2)),
-            ("select 3;", (3)),
-        ]
-    )
-    # simple query
-    query_result = yield from pool.run_query("select %s;", (5,))
-
-    print("morgify_result=", morgify_result)
-    print("transaction_result=", transaction_result)
-    print("query_result=", query_result)
+    pool = yield from aiopg.create_pool(dsn)
+    with (yield from pool.cursor()) as cur:
+        ret = yield from cur.execute("SELECT 1")
+        assert ret == (1,)
 
 
 loop = asyncio.get_event_loop()
