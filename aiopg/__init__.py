b1a15ffe1c5f916076ac107735baf76e1da23bea
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-06-22 13:43:39 +0300

    Add version and version_info to exported public API

--- aiopg/__init__.py
+++ aiopg/__init__.py
@@ -7,7 +7,8 @@ from .cursor import Cursor
 from .pool import create_pool, Pool
 
 
-__all__ = ('connect', 'create_pool', 'Connection', 'Cursor', 'Pool')
+__all__ = ('connect', 'create_pool', 'Connection', 'Cursor', 'Pool',
+           'version', 'version_info')
 
 __version__ = '0.3.0'
 
