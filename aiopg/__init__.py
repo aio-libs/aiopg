d7d28fffa67ca5a485394d3ea73b4c9f7104f04f
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-10-02 12:20:39 +0300

    Bump 0.4.0

--- aiopg/__init__.py
+++ aiopg/__init__.py
@@ -10,7 +10,7 @@ from .pool import create_pool, Pool
 __all__ = ('connect', 'create_pool', 'Connection', 'Cursor', 'Pool',
            'version', 'version_info', 'DEFAULT_TIMEOUT')
 
-__version__ = '0.4.0a0'
+__version__ = '0.4.0'
 
 version = __version__ + ' , Python ' + sys.version
 
