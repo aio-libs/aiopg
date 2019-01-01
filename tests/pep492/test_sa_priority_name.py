610d69170cb32a1db296bcdc6dc6eb5ce3d6822d
Alexey Firsov <alexey.firsov@exness.com>
2019-01-01 12:08:13 +0200

    Fix select map priority name (key)

---
+++ tests/pep492/test_sa_priority_name.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+
+import pytest
+import sqlalchemy as sa
+
+meta = sa.MetaData()
+tbl = sa.Table(
+    'sa_tbl5', meta,
+    sa.Column('ID', sa.String, primary_key=True, key='id'),
+    sa.Column('Name', sa.String(255), key='name'),
+)
+
+
+@pytest.fixture
+def connect(make_sa_connection, loop):
+    async def start():
+        conn = await make_sa_connection()
+        await conn.execute('DROP TABLE IF EXISTS sa_tbl5')
+        await conn.execute(
+            'CREATE TABLE sa_tbl5 ('
+            '"ID" VARCHAR(255) NOT NULL, '
+            '"Name" VARCHAR(255), '
+            'PRIMARY KEY ("ID"))'
+        )
+
+        return conn
+
+    return loop.run_until_complete(start())
+
+
+async def test_priority_name(connect):
+    await connect.execute(tbl.insert().values(id='test_id', name='test_name'))
+    row = await (await connect.execute(tbl.select())).fetchone()
+    assert row.name == 'test_name'
+    assert row.id == 'test_id'
