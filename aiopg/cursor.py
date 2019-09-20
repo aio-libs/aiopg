7aa0e182a109765935ccb79c7ebc3113e698ffde
Alexey Firsov <virmir49@gmail.com>
2019-09-20 13:21:11 +0300

    Update aiopg/cursor.py
    Co-Authored-By: Boryslav Larin <brabadu@gmail.com>

--- aiopg/cursor.py
+++ aiopg/cursor.py
@@ -39,7 +39,7 @@ class Cursor:
         6.  null_ok: always None as not easy to retrieve from the libpq.
 
         This attribute will be None for operations that do not
-        anreturn rows or if the cursor has not had  operation invoked
+        return rows or if the cursor has not had an operation invoked
         via the execute() method yet.
 
         """
