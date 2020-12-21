a65e032cec095da18e38963581bede9d5a6f3b81
Yury Pliner <yury.pliner@gmail.com>
2020-12-21 10:30:48 +0500

    Add tests

--- tests/test_isolation_level.py
+++ tests/test_isolation_level.py
@@ -11,3 +11,58 @@ from aiopg import IsolationLevel
 ])
 def test_isolation_level_name(isolation_level, name):
     assert isolation_level(False, False).name == name
+
+
+@pytest.mark.parametrize(
+    'isolation_level, readonly, deferred, expected_begin',
+    [
+        (IsolationLevel.default, False, False, 'BEGIN'),
+        (IsolationLevel.default, True, False, 'BEGIN READ ONLY'),
+        (
+                IsolationLevel.read_committed,
+                False,
+                False,
+                'BEGIN ISOLATION LEVEL READ COMMITTED'
+        ),
+        (
+                IsolationLevel.read_committed,
+                True,
+                False,
+                'BEGIN ISOLATION LEVEL READ COMMITTED READ ONLY'
+        ),
+        (
+                IsolationLevel.repeatable_read,
+                False,
+                False,
+                'BEGIN ISOLATION LEVEL REPEATABLE READ'
+        ),
+        (
+                IsolationLevel.repeatable_read,
+                True,
+                False,
+                'BEGIN ISOLATION LEVEL REPEATABLE READ READ ONLY'
+        ),
+        (
+                IsolationLevel.serializable,
+                False,
+                False,
+                'BEGIN ISOLATION LEVEL SERIALIZABLE'
+        ),
+        (
+                IsolationLevel.serializable,
+                True,
+                False,
+                'BEGIN ISOLATION LEVEL SERIALIZABLE READ ONLY'
+        ),
+        (
+                IsolationLevel.serializable,
+                True,
+                True,
+                'BEGIN ISOLATION LEVEL SERIALIZABLE READ ONLY DEFERRABLE'
+        ),
+    ]
+)
+def test_isolation_level_begin(
+    isolation_level, readonly, deferred, expected_begin
+):
+    assert isolation_level(readonly, deferred).begin() == expected_begin
