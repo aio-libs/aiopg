5e52725fab3139b65fa2c6e0187cbc0a3d745194
Anton Ostapenko <olsnod@gmail.com>
2021-11-01 13:38:19 +0500

    Add support for Python 3.10 (#884)

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Operating System :: POSIX",
     "Operating System :: MacOS :: MacOS X",
     "Operating System :: Microsoft :: Windows",
